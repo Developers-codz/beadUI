08c5f66d288f00a02036db30adcdcfd588480fa9
developers-codz <kaj123al2002@gmail.com>
2022-01-31 00:57:44 +0530

    feat: rating icon added

--- css/component.js
+++ css/component.js
@@ -4,3 +4,11 @@ var removableAlert = document.querySelector(".removable-alert");
 removeBtn.addEventListener("click",()=>{
     removableAlert.style.display="none"
 })
+
+// card
+
+var cardDismissed = document.querySelector(".card-dismiss")
+var removeCard = document.querySelector(".dismiss-card-btn")
+removeCard.addEventListener("click",()=>{
+    cardDismissed.style.display = "none"
+})

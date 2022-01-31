7704df808d68d0756046ba0cd7508763a218c5d3
developers-codz <kaj123al2002@gmail.com>
2022-01-31 22:01:47 +0530

    feat: card page done

---
+++ docsite/components/alert.js
@@ -0,0 +1,8 @@
+// alert
+
+var removeBtn = document.querySelector(".remove-alert-btn");
+var removableAlert = document.querySelector(".removable-alert");
+
+removeBtn.addEventListener("click",()=>{
+    removableAlert.style.display="none"
+})
